434a5a6e3405184e7bc2596d7a0344d1542094c1
vamsi-git-test <vamsikadaru01@gmail.com>
2024-07-23 16:07:26 +0530

    Hello World Bean

--- src/main/java/com/vamsi/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/vamsi/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.vamsi.rest.webservices.restful_web_services.helloworld;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +12,13 @@ public class HelloWorldController {
 	
 	//"Hello World"
 	
-	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+	
+	@GetMapping(path = "/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hello World");
 	}
 }
