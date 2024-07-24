8b2d1b30d999d9ac6101046914e58d77d2e1d1d0
vamsi-git-test <vamsikadaru01@gmail.com>
2024-07-24 12:02:34 +0530

    Set a path variable

--- src/main/java/com/vamsi/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/vamsi/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.vamsi.rest.webservices.restful_web_services.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,4 +22,12 @@ public class HelloWorldController {
 	public HelloWorldBean helloWorldBean() {
 		return new HelloWorldBean("Hello World");
 	}
+	
+	// Path Parameters => /users/2/totos/48
+	
+	@GetMapping(path = "/hello-world/path-variable/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
 }
