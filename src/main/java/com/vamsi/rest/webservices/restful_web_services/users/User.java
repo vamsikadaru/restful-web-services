58920ad0c65d9d62cbb087f9c49622a3d46f6ef6
vamsikadaru <vamsikadaru01@gmail.com>
2024-07-30 22:44:41 +0530

    Dynamic Filtering

--- src/main/java/com/vamsi/rest/webservices/restful_web_services/users/User.java
+++ src/main/java/com/vamsi/rest/webservices/restful_web_services/users/User.java
@@ -1,6 +1,7 @@
 package com.vamsi.rest.webservices.restful_web_services.users;
 
 import java.time.LocalDate;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
@@ -10,9 +11,11 @@ import jakarta.validation.constraints.Size;
 		private Integer id;
 		
 		@Size(min=2, message = "Name should have atleast 2 Characters")
+		@JsonProperty("user_name")
 		private String name;
 		
 		@Past(message= "Birth Date should be in the past")
+		@JsonProperty("birth_date")
 		private LocalDate birthDate;
 		
 		public User(Integer id, String name, LocalDate birthDate) {
