b886442b7ceab11c9bd5ea609401d4cf10357114
vamsikadaru <vamsikadaru01@gmail.com>
2024-07-30 23:02:11 +0530

    Dynamic Filtering

--- src/main/java/com/vamsi/rest/webservices/restful_web_services/filtering/SomeBean.java
+++ src/main/java/com/vamsi/rest/webservices/restful_web_services/filtering/SomeBean.java
@@ -1,13 +1,15 @@
 package com.vamsi.rest.webservices.restful_web_services.filtering;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 //@JsonIgnoreProperties({"field1", "field2"})
+@JsonFilter("SomeBeanFilter")
 public class SomeBean {
 	private String field1;
 	
-	@JsonIgnore
+//	@JsonIgnore
 	private String field2;
 	
 	private String field3;
