c8d3518475328b77347d6cf1dfe007eb51752cb8
vamsi-git-test <vamsikadaru01@gmail.com>
2024-07-30 17:56:06 +0530

    i18n

--- src/main/java/com/vamsi/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
+++ src/main/java/com/vamsi/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -1,5 +1,9 @@
 package com.vamsi.rest.webservices.restful_web_services.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 //REST API
 @RestController
 public class HelloWorldController {
-	//url: /hello-world
 	
-	//"Hello World"
+	
+	private MessageSource messageSource;
+	
+	public HelloWorldController(MessageSource messageSource) {
+		this.messageSource=messageSource;
+	}
 	
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
@@ -32,7 +40,10 @@ public class HelloWorldController {
 	
 	@GetMapping(path = "/hello-world-internationalized")
 	public String helloWorldInternationalized() {
-		return "Hello World V2";
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+		
+		//return "Hello World V2";
 	}
 	
 }
